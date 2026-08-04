4621f1bf4aa7e7f40c8a7eb72674c89171924f70
DifolderyXXL <87066000+DifolderyXXL@users.noreply.github.com>
2026-08-04 13:16:54 +0300

    Fix reservation cancellation and expiration force levels. Fixed dead reservations

--- Backend/ServicesAPI/Application/Scheduling/IReservationService.cs
+++ Backend/ServicesAPI/Application/Scheduling/IReservationService.cs
@@ -9,5 +9,5 @@ public interface IReservationService
     Task<ReservedTimeWindow> TryReserve(Guid doctorId, Guid appointmentId, ScheduleTimeWindow scheduleTimeWindow, CancellationToken ct);
     Task<bool> TryConfirmReservation(long reservationId, CancellationToken ct);
     Task<IEnumerable<ScheduleTimeWindow>> GetAvailablePositionsOnDay(Guid doctorId, DateOnly date, CancellationToken ct);
-    Task CancelReservation(long reservationId, CancellationToken ct);
+    Task CancelReservation(long reservationId, bool force, CancellationToken ct);
 }

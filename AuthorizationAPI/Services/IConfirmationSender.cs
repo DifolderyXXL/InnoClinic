05740c5d9e8eb70f245b383cccd4f145923ae068
DifolderyXXL <87066000+DifolderyXXL@users.noreply.github.com>
2026-08-13 14:52:59 +0300

    refactor: removed unused project

--- AuthorizationAPI/Services/IConfirmationSender.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Microsoft.AspNetCore.Identity;
-
-namespace AuthorizationAPI.Services;
-
-public interface IConfirmationSender
-{
-    public Task SendConfirmation(IdentityUser user, string token, bool useCookies, bool rememberMe);
-}
--- AuthorizationAPI/Services/ISessionAuthorizeManager.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace AuthorizationAPI.Services;
-
-public interface ISessionAuthorizeManager
-{
-    public Task<bool> AuthorizeSession(IdentityUser user, bool useCookies, bool rememberMe);
-}
